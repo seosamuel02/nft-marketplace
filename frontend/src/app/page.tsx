9d8f2033a21e383a5358b085befb0c30c2f6cb1c
seosamuel02 <tjehd10613@naver.com>
2025-12-07 01:12:44 +0900

    Update Student ID and Name

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -213,6 +213,12 @@ export default function Home() {
         </div>
       </header>
 
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Student ID: 92113669&nbsp;|&nbsp;Name: 서동민
+        </p>
+      </div>
+
       <div className="max-w-7xl mx-auto px-6 space-y-12">
 
         {/* Info Section */}
